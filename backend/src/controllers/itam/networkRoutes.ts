6b7557fcab7191430dd795c03522ebb3a90b2554
o0Poseidon0o <90851953+o0Poseidon0o@users.noreply.github.com>
2026-02-11 09:12:39 +0700

    upload

--- backend/src/controllers/itam/networkRoutes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { scanNetwork } from "../../controllers/itam/networkScanController";
-
-const router = express.Router();
-
-// POST /api/itam/network/scan
-router.post("/scan", scanNetwork);
-
-export default router;
